33339a060540aca8a24db762d4ff6c71493c5ebd
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-02 00:34:02 +0300

    Add: Linear Programs task 01 finished

--- src/com/company/LinearPrograms/Task01.java
+++ src/com/company/LinearPrograms/Task01.java
@@ -1,10 +1,14 @@
 //Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
-package LinearPrograms;
+package com.company.LinearPrograms;
 
 import java.util.Scanner;
 
 public class Task01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(((a - 3) * b / 2) + c);
     }
 }
