ff7b764d66400ef9638b9d118a93f1914c0f34a8
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-10 19:49:21 +0300

    Add: Branching Task 2 finished

--- src/com/company/Branching/Task2.java
+++ src/com/company/Branching/Task2.java
@@ -1,4 +1,12 @@
+//Найти max{min(a, b), min(c, d)}.
 package com.company.Branching;
 
 public class Task2 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        int c = 33;
+        int d = 2;
+        System.out.println(Math.max((Math.min(a, b)), Math.min(c, d)));
+    }
 }
