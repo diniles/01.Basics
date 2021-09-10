f809f1527468f0d8d3db510e7b776c40147fd530
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-10 19:58:14 +0300

    Add: Branching Task 3 finished

--- src/com/company/Branching/Task3.java
+++ src/com/company/Branching/Task3.java
@@ -1,4 +1,16 @@
+//Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). Определить, будут ли они расположены на одной прямой.
 package com.company.Branching;
 
 public class Task3 {
+    public static void main(String[] args) {
+        int ax1 = 1;
+        int ay1 = 1;
+        int ax2 = 3;
+        int ay2 = 2;
+        int ax3 = 3;
+        int ay3 = 3;
+
+        System.out.println((ax3 * (ay2 - ay1) - ay3 * (ax2 - ax1) == ax1 * ay2 - ax2 * ay1));
+
+    }
 }
