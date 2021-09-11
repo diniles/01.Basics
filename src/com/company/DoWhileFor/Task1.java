e2fe8b59640e176973598b1a800fcfd711426ca8
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-11 20:12:09 +0300

    Add: DoWhileFor task 1 finished

--- src/com/company/DoWhileFor/Task1.java
+++ src/com/company/DoWhileFor/Task1.java
@@ -2,8 +2,16 @@
 //все числа от 1 до введенного пользователем числа.
 package com.company.DoWhileFor;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
