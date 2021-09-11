a8cc3002b7b0f7e62b24cc5ba7da1fd6b47c4131
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-11 19:59:45 +0300

    Add: DoWhileFor task 1

---
+++ src/com/company/DoWhileFor/Task1.java
@@ -0,0 +1,9 @@
+//Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует
+//все числа от 1 до введенного пользователем числа.
+package com.company.DoWhileFor;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
