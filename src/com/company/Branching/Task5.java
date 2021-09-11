ba00e593da09b3e3961375558a0eb44082351267
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-11 15:55:55 +0300

    Add: Branching Task 5 finished (fixed warning)

--- src/com/company/Branching/Task5.java
+++ src/com/company/Branching/Task5.java
@@ -4,4 +4,14 @@
 package com.company.Branching;
 
 public class Task5 {
+    public static void main(String[] args) {
+        double x = 4;
+        double result;
+        if (x <= 3) {
+            result = (Math.pow(x, 2) - 3 * x + 9);
+        } else {
+            result = 1 / (x * x + 6);
+        }
+        System.out.println(result);
+    }
 }
