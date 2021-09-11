cfe6c60611c81f9cfde07534f8d760f907131980
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-11 20:19:28 +0300

    Add: DoWhileFor task 4 finished

--- src/com/company/DoWhileFor/Task4.java
+++ src/com/company/DoWhileFor/Task4.java
@@ -2,4 +2,11 @@
 package com.company.DoWhileFor;
 
 public class Task4 {
+    public static void main(String[] args) {
+        int mul = 1;
+        for (int i = 1; i < 200; i++) {
+            mul *= Math.pow(i, 2);
+        }
+        System.out.println(mul);
+    }
 }
