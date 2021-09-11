5628f87d12184d7540ea68f84bc455a933c4d478
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-11 20:16:42 +0300

    Add: DoWhileFor task 3 finished

--- src/com/company/DoWhileFor/Task3.java
+++ src/com/company/DoWhileFor/Task3.java
@@ -2,4 +2,11 @@
 package com.company.DoWhileFor;
 
 public class Task3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 100; i++) {
+            sum += i * i;
+        }
+        System.out.println(sum);
+    }
 }
