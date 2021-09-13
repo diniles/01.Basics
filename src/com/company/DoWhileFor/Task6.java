48f7dcd5880f4a02b2dc73f401184eb452232ed7
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 20:02:35 +0300

    Add: DoWhileFor Task 6 finished

--- src/com/company/DoWhileFor/Task6.java
+++ src/com/company/DoWhileFor/Task6.java
@@ -4,6 +4,8 @@ package com.company.DoWhileFor;
 
 public class Task6 {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 255; i++) {
+            System.out.println(i + " - " + (char) i);
+        }
     }
 }
