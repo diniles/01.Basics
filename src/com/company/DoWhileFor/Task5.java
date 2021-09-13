afed72ccadb445d304b58355cf1f7c4389e1e218
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 19:45:56 +0300

    Add: DoWhileFor Task 5

---
+++ src/com/company/DoWhileFor/Task5.java
@@ -0,0 +1,10 @@
+//Даны числовой ряд и некоторое число е.Найти сумму тех членов ряда,модуль которых больше или равен
+//заданному е.Общий член ряда имеет вид: a(n)=(1/pow(2,n)+(1/pow(3,n)))
+
+package com.company.DoWhileFor;
+
+public class Task5 {
+    public static void main(String[] args) {
+        
+    }
+}
