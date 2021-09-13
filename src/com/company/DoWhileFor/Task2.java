b00b3ae141868252c771d6d335c457498a7ae234
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 20:46:43 +0300

    Add: DoWhileFor Task 2 finished

--- src/com/company/DoWhileFor/Task2.java
+++ src/com/company/DoWhileFor/Task2.java
@@ -2,4 +2,16 @@
 package com.company.DoWhileFor;
 
 public class Task2 {
+    public static void main(String[] args) {
+        int a = -7;
+        int b = 12;
+        int h = 3;
+        for (int i = a; i <= b; i = i + h) {
+            if (i <= 2) {
+                System.out.println("x= " + " " + i + " y= " + -i);
+            } else {
+                System.out.println("x= " + " " + i + " y= " + i);
+            }
+        }
+    }
 }
