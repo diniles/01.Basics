a9128ccd3a946f25f97a162d93617b4ee57c73fd
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 19:48:23 +0300

    Add: DoWhileFor Task 8

---
+++ src/com/company/DoWhileFor/Task8.java
@@ -0,0 +1,8 @@
+//Даны два числа. Определить цифры, входящие в запись как первого так и второго числа
+package com.company.DoWhileFor;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+    }
+}
