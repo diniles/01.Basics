17b499cf653e2120cbdb04c01220828e095a4ef9
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 19:47:42 +0300

    Add: DoWhileFor Task 7

---
+++ src/com/company/DoWhileFor/Task7.java
@@ -0,0 +1,9 @@
+//Для каждого натурального числа в промежутке от m до n вывести все делители, кроме единицы и самого числа.
+//m и n вводятся с клавиатуры
+package com.company.DoWhileFor;
+
+public class Task7 {
+    public static void main(String[] args) {
+        
+    }
+}
