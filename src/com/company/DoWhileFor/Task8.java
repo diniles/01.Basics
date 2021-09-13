12ac2b0ca38d158619859e2c1c9e611d3bc62bd1
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 21:19:12 +0300

    Add: DoWhileFor Task 8 finished

--- src/com/company/DoWhileFor/Task8.java
+++ src/com/company/DoWhileFor/Task8.java
@@ -3,6 +3,19 @@ package com.company.DoWhileFor;
 
 public class Task8 {
     public static void main(String[] args) {
-
+        int a = 124566;
+        int b = 678562;
+        int[] arr = new int[10];
+        while (a != 0) {
+            arr[a % 10]++;
+            a /= 10;
+        }
+        while (b != 0) {
+            if (arr[b % 10] != 0)
+                System.out.println(b % 10);
+            b /= 10;
+        }
     }
 }
+
+
