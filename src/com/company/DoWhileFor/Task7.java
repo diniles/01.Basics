4b424f96ce6fc560a2ad7ed1cbf31dca7c4fa11c
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 19:57:18 +0300

    Add: DoWhileFor Task 7 finished

--- src/com/company/DoWhileFor/Task7.java
+++ src/com/company/DoWhileFor/Task7.java
@@ -2,8 +2,23 @@
 //m и n вводятся с клавиатуры
 package com.company.DoWhileFor;
 
+import java.util.Scanner;
+
 public class Task7 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        for (int i = m; i <= n; i++) {
+            System.out.println("Number i: " + " " + i);
+            System.out.print("Dividers: ");
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
     }
 }
