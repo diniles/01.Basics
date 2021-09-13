9f0778bf81f0efd9c10f19b35821d0eba4ee32e7
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-13 21:29:10 +0300

    Add: DoWhileFor Task 5 finished

--- src/com/company/DoWhileFor/Task5.java
+++ src/com/company/DoWhileFor/Task5.java
@@ -1,10 +1,20 @@
-//Даны числовой ряд и некоторое число е.Найти сумму тех членов ряда,модуль которых больше или равен
+//Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда,модуль которых больше или равен
 //заданному е.Общий член ряда имеет вид: a(n)=(1/pow(2,n)+(1/pow(3,n)))
 
 package com.company.DoWhileFor;
 
 public class Task5 {
     public static void main(String[] args) {
-        
+        double start = 2.0;
+        double end = 12.0;
+        double e = 0.123;
+        double sum = 0.0;
+        for (double i = start; i <= end; i++) {
+            double result = (1 / Math.pow(2, i)) + (1 / Math.pow(3, i));
+            if (Math.abs(result) >= Math.abs(e)) {
+                sum += result;
+            }
+        }
+        System.out.println(sum);
     }
 }
